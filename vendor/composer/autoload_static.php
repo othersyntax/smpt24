dcbee2fa4c323861898cdba076e7fedf0507a7ad
ridzuan1985 <onemuc@gmail.com>
2024-11-07 13:05:00 +0800

    apply 13 changes

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -340,8 +340,8 @@ class ComposerStaticInite7d2622b504198316e6986684c8f3bcc
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c4ce3bc69894d369b903ccab742ec520fa1a7d3b',
+        'reference' => 'bed4462b7dc9c388bf54c85b519b42ac9f019169',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -460,7 +460,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c4ce3bc69894d369b903ccab742ec520fa1a7d3b',
+            'reference' => 'bed4462b7dc9c388bf54c85b519b42ac9f019169',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -853,8 +853,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0',
             ),
         ),
         'psr/simple-cache' => array(
