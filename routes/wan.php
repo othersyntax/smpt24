f65bc61ddfd25dffc09cdf0c2cbb15f75f34fbed
ridzuan1985 <onemuc@gmail.com>
2024-11-07 15:42:55 +0800

    new modul syarikat

--- routes/wan.php
+++ routes/wan.php
@@ -1,3 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SyarikatController;
+
+Route::prefix("/utiliti/syarikat")->middleware('isLoggedIn')->group(function(){
+    Route::any('/senarai',[SyarikatController::class, 'index']);
+    // Route::post('/simpan',[SyarikatController::class, 'simpan']);
+    // Route::post('/ubah',[SyarikatController::class, 'ubah']);
+});
